a1c53df3837003aa008ef685fa29e479e139b9af
Paweł Marczewski <pwmarcz@gmail.com>
2018-10-31 13:12:46 +0100

    controller: receive commands in a more generic way

--- memory_controller.v
+++ memory_controller.v
@@ -12,26 +12,28 @@ module memory_controller(input wire       clk,
 
   reg         read;
   reg         write;
-  reg [1:0]   command;
+  reg [7:0]   command_bytes[0:3];
+  reg [1:0]   command_idx;
+
+  wire [1:0]  command = command_bytes[0][1:0];
   reg [15:0]  addr;
   reg [7:0]   count;
+
   reg [7:0]   write_byte;
   wire [7:0]  read_byte;
 
   memory mem(clk, read, write, addr, write_byte, read_byte);
 
-  localparam [4:0]
+  localparam [3:0]
     STATE_IDLE = 0,
-    STATE_RX_COUNT = 1,
-    STATE_RX_ADDR_HI = 2,
-    STATE_RX_ADDR_LO = 3,
-    STATE_READ_WAIT = 4,
-    STATE_READ = 5,
-    STATE_READ_ADVANCE = 6,
-    STATE_WRITE_RX_DATA = 7,
-    STATE_WRITE_ADVANCE = 8;
+    STATE_RX_COMMAND = 1,
+    STATE_READ_WAIT = 2,
+    STATE_READ = 3,
+    STATE_READ_ADVANCE = 4,
+    STATE_WRITE_RX_DATA = 5,
+    STATE_WRITE_ADVANCE = 6;
 
-  reg [4:0] state = STATE_IDLE;
+  reg [2:0] state = STATE_IDLE;
 
   always @(posedge clk)
     begin
@@ -42,43 +44,32 @@ module memory_controller(input wire       clk,
       case (state)
         STATE_IDLE: begin
           if (received) begin
-            case (rx_byte)
-              `COMMAND_READ, `COMMAND_WRITE: begin
-                command <= rx_byte;
-                state <= STATE_RX_COUNT;
-              end
-            endcase
+            command_idx <= 1;
+            command_bytes[0] <= rx_byte;
+            state = STATE_RX_COMMAND;
           end
         end
 
-        STATE_RX_COUNT: begin
+        STATE_RX_COMMAND: begin
           if (received) begin
-            count <= rx_byte;
-            state <= STATE_RX_ADDR_HI;
+            command_bytes[command_idx] = rx_byte;
+
+            if (command_idx < 3) begin
+              command_idx <= command_idx + 1;
+            end else begin
+              addr <= {command_bytes[2], command_bytes[3]};
+              count <= command_bytes[1];
+              case (command)
+                `COMMAND_READ: begin
+                  state <= STATE_READ_WAIT;
+                end
+                `COMMAND_WRITE: begin
+                  state <= STATE_WRITE_RX_DATA;
+                end
+              endcase
+            end // else: !if(command_idx < 3)
           end
         end
-
-        STATE_RX_ADDR_HI: begin
-          if (received) begin
-            addr[15:8] <= rx_byte;
-            state <= STATE_RX_ADDR_LO;
-          end
-        end
-
-        STATE_RX_ADDR_LO: begin
-          if (received) begin
-            addr[7:0] <= rx_byte;
-            command <= 0;
-            case (command)
-              `COMMAND_READ: begin
-                state <= STATE_READ_WAIT;
-              end
-              `COMMAND_WRITE: begin
-                state <= STATE_WRITE_RX_DATA;
-              end
-            endcase
-          end
-        end // case: STATE_RX_ADDR
 
         STATE_READ_WAIT: begin
           if (!is_transmitting) begin
