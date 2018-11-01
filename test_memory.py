1df98e9fb824f697697ee0275735ebe90d188b25
Paweł Marczewski <pwmarcz@gmail.com>
2018-11-01 14:40:27 +0100

    Test GPU through serial line

--- test_memory.py
+++ test_memory.py
@@ -7,6 +7,7 @@ BAUD_RATE = 19200
 
 COMMAND_READ = 1
 COMMAND_WRITE = 2
+COMMAND_DRAW = 3
 
 SIZE = 0x1000
 CHUNK_SIZE = 0x80
@@ -26,21 +27,63 @@ def read(ser, addr, count):
     return data
 
 
-buf = [i % 254 for i in range(SIZE)]
+def draw(ser, sprite_addr, lines, x, y):
+    ser.write([
+        COMMAND_DRAW | (lines << 4),
+        (x << 4) | y,
+        sprite_addr >> 8,
+        sprite_addr & 0xFF
+    ])
 
-with serial.Serial('/dev/ttyUSB1', 19200, timeout=10) as ser:
-    print('Writing')
-    for i in range(0, SIZE, CHUNK_SIZE):
-        write(ser, i, buf[i:i + CHUNK_SIZE])
-        print('.', end='', flush=True)
 
+def test_memory():
+    buf = [i % 254 for i in range(SIZE)]
+
+    with serial.Serial('/dev/ttyUSB1', 19200, timeout=10) as ser:
+        print('Writing')
+        for i in range(0, SIZE, CHUNK_SIZE):
+            write(ser, i, buf[i:i + CHUNK_SIZE])
+            print('.', end='', flush=True)
+
+        print()
+        print('Reading')
+        for i in range(0, SIZE, CHUNK_SIZE):
+            data = read(ser, i, CHUNK_SIZE)
+            expected = buf[i:i + CHUNK_SIZE]
+            assert data == expected, f'read error:\n{data}\n{expected}'
+            print('.', end='', flush=True)
+
+        print()
+        print('Memory test OK')
+
+
+def draw_screen(ser):
+    buf = read(ser, 0x100, 256)
+    print('Screen:')
+    for y in range(32):
+        for x in range(64):
+            byte = buf[y * 8 + x // 8]
+            bit = bool(byte & (1 << (7 - (x % 8))))
+            print('#' if bit else '.', end='')
+        print()
     print()
-    print('Reading')
-    for i in range(0, SIZE, CHUNK_SIZE):
-        data = read(ser, i, CHUNK_SIZE)
-        expected = buf[i:i + CHUNK_SIZE]
-        assert data == expected, f'read error:\n{data}\n{expected}'
-        print('.', end='', flush=True)
 
-    print()
-    print('All OK')
+
+def test_gpu():
+    with serial.Serial('/dev/ttyUSB1', 19200, timeout=10) as ser:
+        write(ser, 0x100, [0] * 256)
+        write(ser, 0x45, [
+            0b00011000,
+            0b01100110,
+            0b01111110,
+            0b01100110,
+            0b01100110,
+        ])
+        draw(ser, 0x45, 5, 4, 4)
+        draw(ser, 0x45, 5, 12, 4)
+        draw_screen(ser)
+
+
+if __name__ == '__main__':
+    test_gpu()
+    #test_memory()
