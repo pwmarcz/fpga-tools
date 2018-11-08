0a1a25cd0f8e7fc36bedfe5d34370c76d47c6aab
Paweł Marczewski <pwmarcz@gmail.com>
2018-11-08 23:08:43 +0100

    Send out data to display

--- display.v
+++ display.v
@@ -6,10 +6,10 @@
 `define CMD_SEND_DATA     2'b11
 
 module display_spi(input wire clk,
-                   input wire [2:0] cmd,
-                   input wire [7:0] tx_byte,
+                   input wire [2:0] dspi_cmd,
+                   input wire [7:0] dspi_byte,
 
-                   output wire      ready,
+                   output wire      dspi_ready,
                    output reg       spi_din,
                    output reg       spi_clk,
                    output reg       spi_cs,
@@ -21,7 +21,7 @@ module display_spi(input wire clk,
 
   reg reset = 0;
   reg send = 0;
-  assign ready = !reset && !send && cmd == `CMD_NONE;
+  assign dspi_ready = !reset && !send && dspi_cmd == `CMD_NONE;
 
   initial begin
     spi_clk = 0;
@@ -31,15 +31,15 @@ module display_spi(input wire clk,
 
   always @(posedge clk) begin
     if (!reset && !send) begin
-      case (cmd)
+      case (dspi_cmd)
         `CMD_RESET: begin
           reset <= 1;
           spi_rst <= 0;
         end
         `CMD_SEND_COMMAND, `CMD_SEND_DATA: begin
           send <= 1;
-          spi_dc <= (cmd == `CMD_SEND_DATA);
-          data <= tx_byte;
+          spi_dc <= (dspi_cmd == `CMD_SEND_DATA);
+          data <= dspi_byte;
           data_counter <= 8;
         end
       endcase
@@ -77,61 +77,114 @@ endmodule // display_spi
 
 
 module display(input wire clk,
-               input wire ready,
-               output reg [2:0] cmd,
-               output reg [7:0] tx_byte);
+               input wire dspi_ready,
+               output reg [2:0] dspi_cmd,
+               output reg [7:0] dspi_byte);
 
-  localparam MAX_COMMAND = 9;
+  localparam MAX_INIT_COMMAND = 24;
+  localparam MAX_REFRESH_COMMAND = 5;
   localparam
     STATE_RESET = 0,
     STATE_INIT = 1,
-    STATE_IDLE = 2;
+    STATE_IDLE = 2,
+    STATE_REFRESH_BEGIN = 3,
+    STATE_REFRESH_DATA = 4;
 
-  reg [7:0] commands[0:MAX_COMMAND];
+  reg [7:0] init_commands[0:MAX_INIT_COMMAND];
+  reg [7:0] refresh_commands[0:MAX_REFRESH_COMMAND];
   reg [8:0] command_idx = 0;
+  reg [2:0] page_idx = 0; // 0..7
+  reg [6:0] column_idx = 0; // 0..127
 
   reg [3:0] state = STATE_RESET;
 
-  initial begin
-    cmd = `CMD_RESET;
+  integer i;
 
-    commands[0] <= `SSD1306_DISPLAYOFF;
-    commands[1] <= `SSD1306_CHARGEPUMP;
-    commands[2] <= 'h14;
-    commands[3] <= `SSD1306_SETCONTRAST;
-    commands[4] <= 'h70;
-    commands[5] <= `SSD1306_SETPRECHARGE;
-    commands[6] <= 'hF1;
-    commands[7] <= `SSD1306_DISPLAYALLON_RESUME;
-    commands[8] <= `SSD1306_NORMALDISPLAY;
-    commands[9] <= `SSD1306_DISPLAYON;
+  initial begin
+    dspi_cmd = `CMD_RESET;
+
+    i = -1;
+    i++; init_commands[i] <= `SSD1306_DISPLAYOFF;
+    i++; init_commands[i] <= `SSD1306_SETDISPLAYCLOCKDIV;
+    i++; init_commands[i] <= 'h80;
+    i++; init_commands[i] <= `SSD1306_SETMULTIPLEX;
+    i++; init_commands[i] <= 'h3F;
+    i++; init_commands[i] <= `SSD1306_SETDISPLAYOFFSET;
+    i++; init_commands[i] <= 'h00;
+    i++; init_commands[i] <= `SSD1306_SETSTARTLINE | 'h00;
+    i++; init_commands[i] <= `SSD1306_CHARGEPUMP;
+    i++; init_commands[i] <= 'h14;
+    i++; init_commands[i] <= `SSD1306_MEMORYMODE;
+    i++; init_commands[i] <= 'h00;
+    i++; init_commands[i] <= `SSD1306_SEGREMAP | 'h01;
+    i++; init_commands[i] <= `SSD1306_COMSCANDEC;
+    i++; init_commands[i] <= `SSD1306_SETCOMPINS;
+    i++; init_commands[i] <= 'h12;
+    i++; init_commands[i] <= `SSD1306_SETCONTRAST;
+    i++; init_commands[i] <= 'h70;
+    i++; init_commands[i] <= `SSD1306_SETPRECHARGE;
+    i++; init_commands[i] <= 'hF1;
+    i++; init_commands[i] <= `SSD1306_SETVCOMDETECT;
+    i++; init_commands[i] <= 'h40;
+    i++; init_commands[i] <= `SSD1306_DISPLAYALLON_RESUME;
+    i++; init_commands[i] <= `SSD1306_NORMALDISPLAY;
+    i++; init_commands[i] <= `SSD1306_DISPLAYON;
+
+    i = -1;
+    i++; refresh_commands[i] <= `SSD1306_COLUMNADDR;
+    i++; refresh_commands[i] <= 0;
+    i++; refresh_commands[i] <= 127;
+    i++; refresh_commands[i] <= `SSD1306_PAGEADDR;
+    i++; refresh_commands[i] <= 0;
+    i++; refresh_commands[i] <= 7;
   end
 
   always @(posedge clk) begin
-    case (state)
-      STATE_RESET: begin
-        cmd <= `CMD_NONE;
-        if (ready) begin
+    dspi_cmd <= `CMD_NONE;
+    if (dspi_ready) begin
+      case (state)
+        STATE_RESET: begin
           state <= STATE_INIT;
         end
-      end
-      STATE_INIT: begin
-        cmd <= `CMD_NONE;
-        if (ready) begin
-          cmd <= `CMD_SEND_COMMAND;
-          tx_byte <= commands[command_idx];
+        STATE_INIT: begin
+          dspi_cmd <= `CMD_SEND_COMMAND;
+          dspi_byte <= init_commands[command_idx];
 
-          if (command_idx < MAX_COMMAND) begin
+          if (command_idx < MAX_INIT_COMMAND) begin
             command_idx <= command_idx + 1;
           end else begin
-            state <= STATE_IDLE;
+            state <= STATE_REFRESH_BEGIN;
+            command_idx <= 0;
           end
         end
-      end
-      STATE_IDLE: begin
-        cmd <= `CMD_NONE;
-      end
-    endcase // case (state)
+        STATE_REFRESH_BEGIN: begin
+          dspi_cmd <= `CMD_SEND_COMMAND;
+          dspi_byte <= refresh_commands[command_idx];
+          if (command_idx < MAX_REFRESH_COMMAND) begin
+            command_idx <= command_idx + 1;
+          end else begin
+            state <= STATE_REFRESH_DATA;
+            page_idx <= 0;
+            column_idx <= 0;
+          end
+        end
+        STATE_REFRESH_DATA: begin
+          dspi_cmd <= `CMD_SEND_DATA;
+          if (page_idx == 0)
+            dspi_byte <= 'hFF;
+          else
+            dspi_byte <= 0;
+          column_idx <= column_idx + 1;
+          if (column_idx == 127) begin
+            page_idx <= page_idx + 1;
+            if (page_idx == 7) begin
+              state <= STATE_REFRESH_BEGIN;
+              command_idx <= 0;
+            end
+          end
+        end
+      endcase // case (state)
+    end // if (dspi_ready)
   end
 endmodule // display
 
@@ -148,14 +201,14 @@ module display_demo(input wire  iCE_CLK,
 	            output wire LED3,
                     output wire LED4);
 
-  wire ready;
-  wire [2:0] cmd;
-  wire [7:0] tx_byte;
+  wire dspi_ready;
+  wire [2:0] dspi_cmd;
+  wire [7:0] dspi_byte;
 
   display_spi dspi(.clk(iCE_CLK),
-                   .ready(ready),
-                   .cmd(cmd),
-                   .tx_byte(tx_byte),
+                   .dspi_ready(dspi_ready),
+                   .dspi_cmd(dspi_cmd),
+                   .dspi_byte(dspi_byte),
                    .spi_din(PIO1_02),
                    .spi_clk(PIO1_03),
                    .spi_cs(PIO1_04),
@@ -163,9 +216,9 @@ module display_demo(input wire  iCE_CLK,
                    .spi_rst(PIO1_06));
 
   display d(.clk(iCE_CLK),
-            .ready(ready),
-            .cmd(cmd),
-            .tx_byte(tx_byte));
+            .dspi_ready(dspi_ready),
+            .dspi_cmd(dspi_cmd),
+            .dspi_byte(dspi_byte));
 
   assign LED0 = 0;
   assign LED1 = 0;
