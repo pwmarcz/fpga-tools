48429f74aeabd4148a3beb6b177273f9b20bb2c4
Paweł Marczewski <pwmarcz@gmail.com>
2018-12-28 22:30:10 +0100

    Add stopwatch example

--- demo/icebreaker_example.v
+++ demo/icebreaker_example.v
@@ -1,5 +1,14 @@
 
 module top(input wire  CLK,
+           output wire P1A1,
+           output wire P1A2,
+           output wire P1A3,
+           output wire P1A4,
+           output wire P1A7,
+           output wire P1A8,
+           output wire P1A9,
+           output wire P1A10,
+
            output wire LED1,
            output wire LED2,
            output wire LED3,
@@ -7,6 +16,9 @@ module top(input wire  CLK,
 
   parameter n = 26;
   reg [n-1:0] clk_counter = 0;
+
+  wire [7:0] ss_top;
+  assign { P1A10, P1A9, P1A8, P1A7, P1A4, P1A3, P1A2, P1A1 } = ss_top;
 
   always @(posedge CLK) begin
     clk_counter <= clk_counter + 1;
@@ -17,4 +29,6 @@ module top(input wire  CLK,
   assign LED2 = clk_counter[n-3];
   assign LED3 = clk_counter[n-2];
   assign LED4 = clk_counter[n-1];
+
+  assign ss_top = 0; //8'b11111111;
 endmodule
